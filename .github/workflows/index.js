4ea6b9012a4374c4a1c8638558085f215a6a5c7a
pavanyarrapragada03 <pavanyerrapragada02@gmail.com>
2025-08-08 12:52:38 +0530

    Delete .github/workflows/index.js

--- .github/workflows/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.end('Hello from nodejs-demo-app\n');
-}).listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
